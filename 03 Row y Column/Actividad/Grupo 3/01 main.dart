77a7bf054fc8703202b59aa724fd5f27e3252ecf
Eliana Specogna <specognaeliana@gmail.com>
2025-04-25 21:32:33 -0300

    Realizando ejemplo 01

---
+++ 03 Row y Column/Actividad/Grupo 3/01 main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Row(
+            children: [
+            Text('Widget 1'),
+            Text('Widget 2'),
+            Icon(Icons.star),
+          ],
+          )
+        ),
+      ),
+    );
+  }
+}
